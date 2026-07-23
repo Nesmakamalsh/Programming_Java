13c60a9da6f1e8afcbc93569141bb82f42ef32c9
Nesmakamalsh <nesmakamalahmedmohamed.shabaan@infineon.com>
2026-07-23 09:53:38 +0000

    1st lab is done

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Programming_JavaxPython
-Syntax, programming concepts and problem solving in Java and Python
- I have 12 hrs of Java learning
+![vars](image-2.png)
+![alt text](image-3.png)
---
+++ src/main.java
@@ -0,0 +1,19 @@
+public class main{
+    public static void main(String[] args){
+        System.out.println("Hallo ich bin Nesma\n");
+        /* 
+        This is how to write a comment
+        */
+       int age = 24;
+       System.out.println("Ich bin " + age + " Jahre alt");
+
+       boolean istNett = true;
+
+       if(istNett){
+        System.out.println("Freut mich dich kennenlernen!");
+       }
+       else{
+        System.out.println("Wir sehen uns noch");
+       }
+    }
+}
